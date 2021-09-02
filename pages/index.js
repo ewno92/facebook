6d9ab55be85c932ff199978f13b6a976f773bcce
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-09-01 23:46:50 -0700

    nextAuth installed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div className="">
       <Head>
@@ -17,4 +19,13 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  //get the user
+  const session = await getSession(context);
+
+  return {
+    props: { session },
+  };
 }
